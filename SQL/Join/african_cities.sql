4b028bf31d4fb490aa6b1c3d5a3b333189aeef0d
Pedro Blas <93939660+pedrobaf@users.noreply.github.com>
2022-12-14 16:24:06 +0400

    Update african_cities.sql

--- SQL/Join/african_cities.sql
+++ SQL/Join/african_cities.sql
@@ -4,11 +4,41 @@ https://www.hackerrank.com/challenges/african-cities/problem
 
 Given the CITY and COUNTRY tables, query the names of all cities where the CONTINENT is 'Africa'.
 
-Note: CITY.CountryCode and COUNTRY.Code are matching key columns.
-
 Input Format
 
 The CITY and COUNTRY tables are described as follows:
+
+City
++-------------+----------+
+| Field       | Type     |
++-------------+----------+
+| ID          | Number   |
+| Name        | varchar2(17)|
+| CountryCode | varchar2(3) |
+| District    | varchar2(20)|
+| Population  | Number   |
++-------------+----------+
+Country
++----------------+-------------+
+| Field          | Type        |
++----------------+-------------+
+| Code           | varchar2(3) |
+| Name           | varchar2(44)|
+| Continent      | varchar2(13)|
+| Region         | varchar2(25)|
+| SurfaceArea    | number      |
+| IndepYear      | varchar2(5) |
+| Population     | number      |
+| LifeExpectancy | varchar2(4) |
+| GNP            | number      |
+| GNPOld         | varchar2(9) |
+| LocalName      | varchar2(44)|
+| GovernmentForm | varchar2(44)|
+| HeadOfState    | varchar2(32)|
+| Capital        | varchar2(4) |
+| Code2          | varchar2(2) |
++----------------+-------------+
+Note: CITY.CountryCode and COUNTRY.Code are matching key columns.
 
 */
 
