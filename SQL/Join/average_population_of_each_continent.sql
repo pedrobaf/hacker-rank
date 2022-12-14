d46b9b01863647f1c85bee995f7ae688f05ff01c
Pedro Blas <93939660+pedrobaf@users.noreply.github.com>
2022-12-14 16:34:31 +0400

    Update average_population_of_each_continent.sql

--- SQL/Join/average_population_of_each_continent.sql
+++ SQL/Join/average_population_of_each_continent.sql
@@ -4,11 +4,43 @@ https://www.hackerrank.com/challenges/average-population-of-each-continent/probl
 
 Given the CITY and COUNTRY tables, query the names of all the continents (COUNTRY.Continent) and their respective average city populations (CITY.Population) rounded down to the nearest integer.
 
-Note: CITY.CountryCode and COUNTRY.Code are matching key columns.
 
 Input Format
 
 The CITY and COUNTRY tables are described as follows:
+
+City
++-------------+----------+
+| Field       | Type     |
++-------------+----------+
+| ID          | Number   |
+| Name        | varchar2(17)|
+| CountryCode | varchar2(3) |
+| District    | varchar2(20)|
+| Population  | Number   |
++-------------+----------+
+Country
++----------------+-------------+
+| Field          | Type        |
++----------------+-------------+
+| Code           | varchar2(3) |
+| Name           | varchar2(44)|
+| Continent      | varchar2(13)|
+| Region         | varchar2(25)|
+| SurfaceArea    | number      |
+| IndepYear      | varchar2(5) |
+| Population     | number      |
+| LifeExpectancy | varchar2(4) |
+| GNP            | number      |
+| GNPOld         | varchar2(9) |
+| LocalName      | varchar2(44)|
+| GovernmentForm | varchar2(44)|
+| HeadOfState    | varchar2(32)|
+| Capital        | varchar2(4) |
+| Code2          | varchar2(2) |
++----------------+-------------+
+
+Note: CITY.CountryCode and COUNTRY.Code are matching key columns.
 
 
 */
