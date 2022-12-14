75f660acd8852677b1ef7232506a2cc9a6555aee
Pedro Blas <93939660+pedrobaf@users.noreply.github.com>
2022-12-14 14:45:56 +0400

    Create select_all.sql

---
+++ SQL/Select/select_all.sql
@@ -0,0 +1,23 @@
+/*
+
+https://www.hackerrank.com/challenges/select-all-sql/problem
+
+Query all columns (attributes) for every row in the CITY table.
+
+The CITY table is described as follows:
+
++-------------+--------------+
+| Field       | Type         |
++-------------+--------------+
+| ID          | NUMBER       |
+| NAME        | VARCHAR2(17) |
+| COUNTRYCODE | VARCHAR2(3)  |
+| DISTRICT    | VARCHAR2(20) |
+| POPULATION  | NUMBER       |
++-------------+--------------+
+
+*/
+
+# SOLUTION - MY SQL
+
+SELECT * FROM CITY;
