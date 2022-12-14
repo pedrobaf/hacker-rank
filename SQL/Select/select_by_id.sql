0744f621775de7a77c214d1d33ad8c54e87dc563
Pedro Blas <93939660+pedrobaf@users.noreply.github.com>
2022-12-14 14:48:15 +0400

    Create select_by_id.sql

---
+++ SQL/Select/select_by_id.sql
@@ -0,0 +1,24 @@
+/*
+
+https://www.hackerrank.com/challenges/select-by-id/problem
+
+Query all columns for a city in CITY with the ID 1661.
+
+The CITY table is described as follows:
+
++-------------+--------------+
+| Field       | Type         |
++-------------+--------------+
+| ID          | NUMBER       |
+| NAME        | VARCHAR2(17) |
+| COUNTRYCODE | VARCHAR2(3)  |
+| DISTRICT    | VARCHAR2(20) |
+| POPULATION  | NUMBER       |
++-------------+--------------+
+
+*/
+
+# SOLUTION - MYSQL
+
+SELECT * FROM CITY
+WHERE ID = 1661;
