b0226e1d05d80c73842778c336bdd151f4c8b771
Pedro Blas <93939660+pedrobaf@users.noreply.github.com>
2022-12-14 14:27:04 +0400

    Create weather_observation_station_11.sql

---
+++ SQL/Select/weather_observation_station_11.sql
@@ -0,0 +1,29 @@
+/*
+
+https://www.hackerrank.com/challenges/weather-observation-station-11/problem
+
+Query the list of CITY names from STATION that either do not start with vowels or do not end with vowels. Your result cannot contain duplicates.
+
+Input Format
+
+The STATION table is described as follows:
+
++-------------+------------+
+| Field       |   Type     |
++-------------+------------+
+| ID          | NUMBER     |
+| CITY        | VARCHAR(21)|
+| STATE       | VARCHAR(2) |
+| LAT_N       | NUMBER     |
+| LONG_W      | NUMBER     |
++-------------+------------+
+
+where LAT_N is the northern latitude and LONG_W is the western longitude.
+
+*/
+
+# SOLUTION - MYSQL
+
+SELECT DISTINCT CITY 
+FROM STATION 
+WHERE LEFT(CITY,1) NOT IN ('a','e','i','o','u') OR RIGHT(CITY,1) NOT IN ('a','e','i','o','u');
